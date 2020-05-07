00e22a3af45e92768867e1da95966cfc62fc0013
Nathaniel Ryan <nathanieljryan1994@gmail.com>
2020-05-06 21:12:26 -0700

    GraphQL Yoga server is running GUI displaying with schema on localhost:4444

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,12 @@
+// This file if for communication with Prisma cloud DB and translating 
+// queries to JavaScript
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false,
+})
+
+module.exports = db;
