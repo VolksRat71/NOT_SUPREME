8bfb112b759ab5d5ba29c0b0fb1816f29b0a30eb
VolksRat71 <nathanieljryan1994@gmail.com>
2020-12-01 17:48:38 -0600

    Query and Mutation working with temporary 'database' (array)

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const mutations = {
+    createDog(parent, args, ctx, info) {
+        global.dogs = global.dogs || [];
+
+        const newDog = { name: args.name }
+        global.dogs.push(newDog)
+        return newDog
+    }
+};
 
 module.exports = mutations;
