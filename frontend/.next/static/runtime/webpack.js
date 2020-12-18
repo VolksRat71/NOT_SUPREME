8a85d90cd83842110886507f501f8292309926b0
VolksRat71 <nathanieljryan1994@gmail.com>
2020-12-17 18:34:21 -0600

    Pushing local changes, I must've forgotton to last week

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0059f76d93beb76fbe9e";
+/******/ 	var hotCurrentHash = "cd4025ad2d04b51d992b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
