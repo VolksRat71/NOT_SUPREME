8fc7c63d640913da01d4901daab46d647629433d
VolksRat71 <nathanieljryan1994@gmail.com>
2021-01-09 19:10:01 -0600

    making concurrently a regular dependancy, not a dev dependency

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fb05548d1d9a95fabdfd";
+/******/ 	var hotCurrentHash = "974de372725f924e62f7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
