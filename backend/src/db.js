1479fdb61955523bd264dcc2a692b5705697b672
VolksRat71 <nathanieljryan1994@gmail.com>
2021-01-09 19:16:55 -0600

    trying to add an endpoint

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,8 +3,8 @@ const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: process.env.PRISMA_ENDPOINT,
-    secret: process.env.PRISMA_SECRET,
+    endpoint: 'https://not-supreme-300f69abc5.herokuapp.com/not-supreme-backend/prod',
+    secret: "shhhh",
     debug: false,
 })
 
